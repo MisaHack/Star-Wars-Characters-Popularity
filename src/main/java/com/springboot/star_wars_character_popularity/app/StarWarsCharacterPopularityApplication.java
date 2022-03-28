1df24684b22ce738a21046d0553f337bc320b257
lihvarcekmisa <lihvarcekmisa@yahoo.com>
2022-03-28 15:03:35 +0200

    1st commit - only Main App code

---
+++ src/main/java/com/springboot/star_wars_character_popularity/app/StarWarsCharacterPopularityApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.star_wars_character_popularity.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsCharacterPopularityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsCharacterPopularityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/star_wars_character_popularity/app/StarWarsCharacterPopularityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.star_wars_character_popularity.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsCharacterPopularityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
